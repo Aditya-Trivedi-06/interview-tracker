da9c5d9b9e8d5788e2906c0c2c7f70da4bf527dc
Aditya Trivedi <adityatrivedi7679@gmail.com>
2026-05-29 03:57:40 +0530

    Fix Service Worker caching strategy to allow app updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'faang-tracker-v1';
+const CACHE_NAME = 'faang-tracker-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -6,19 +6,45 @@ const ASSETS = [
   './app.js',
   './data.js',
   './data_revision.js',
-  './manifest.json'
+  './sync.js',
+  './manifest.json',
+  './revision.html'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
+  // Network-first strategy
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        // Clone the response and update the cache in the background
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, resClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
